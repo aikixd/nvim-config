954392141f0651bbb9eaaf726afda63c7ca049ad
Dmitriy Sorkin <aikixd@gmail.com>
2023-07-13 07:51:06 +0300

    .

---
+++ lua/util.lua
@@ -0,0 +1,13 @@
+local M = { }
+
+function M.key_canon_to_lazy(map)
+  return { map.lhs, map.rhs, mode = map.mode, desc = map.opts.desc } 
+end
+
+function M.map(tbl, fn)
+  local r = {}
+  for _, x in ipairs(tbl) do table.insert(r, fn(x)) end
+  return r
+end
+
+return M
